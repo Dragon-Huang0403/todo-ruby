e86000e06b9ef44663c0212007c4c0934fda45f4
Dragon Huang <dragon.huang@shopline.com>
2024-10-02 07:57:16 +0000

    feat: init aritcle

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  rescue Mongoid::Errors::DocumentNotFound
+    flash[:alert] = "Article not found"
+    render json: { error: 'Article not found' }, status: :not_found
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def article_params
+      puts ">>>> params start"
+      puts params
+      puts ">>>> params end"
+      params.require(:article).permit(:title, :body)
+    end
+end
